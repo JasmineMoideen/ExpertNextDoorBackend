f9dde1e87d40922245aa7064ca4e6a54b2497541
jasmine6983 <jasmine.m@techversantinfotech.com>
2025-07-21 13:09:01 +0530

    final updates

--- wp-config.php
+++ wp-config.php
@@ -89,9 +89,11 @@ $table_prefix = 'wp_';
  */
 define( 'WP_DEBUG', true );
 define('WP_DEBUG_DISPLAY', false);
-define('WP_DEBUG_LOG', true); 
+define('WP_DEBUG_LOG', false); 
 /* Add any custom values between this line and the "stop editing" line. */
 define('FS_METHOD', 'direct');
+
+
 
 
 
