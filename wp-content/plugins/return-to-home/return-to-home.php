b11c05a773cdd7e8ef930eb8b488f926309e4ce0
jasmine6983 <jasmine.m@techversantinfotech.com>
2025-08-08 16:38:31 +0530

    return to home in admin bar

--- wp-content/plugins/return-to-home/return-to-home.php
+++ wp-content/plugins/return-to-home/return-to-home.php
@@ -31,3 +31,15 @@ add_action('admin_bar_menu', function($wp_admin_bar) {
         $wp_admin_bar->remove_node('view-site'); // Removes "Visit Site" link
     }
 }, 999);
+
+
+add_action('admin_bar_menu', function($admin_bar) {
+    if (is_user_logged_in()) {
+        $admin_bar->add_menu([
+            'id'    => 'return-to-home',
+            'title' => 'Return to Home',
+            'href'  => 'https://expert-next-door.vercel.app/',
+            'meta'  => ['target' => '_blank']
+        ]);
+    }
+}, 100);
